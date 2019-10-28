0a4fd86fe184b61cafbf1c000264ea3f41d637ec
Markus Felten <Markus.Felten@gmx.de>
2019-10-28 18:49:17 +0100

    feat: journal_logger

--- src/service.mjs
+++ src/service.mjs
@@ -1,8 +1,19 @@
 import { createRequire } from "module";
-import { ServiceProviderMixin, Service } from "@kronos-integration/service";
+import { ServiceProviderMixin, Service, ServiceLogger } from "@kronos-integration/service";
 
 const require = createRequire(import.meta.url);
-const { notify } = require("../systemd.node");
+const { notify, journal_print } = require("../systemd.node");
+
+
+class JournalLogger extends ServiceLogger {
+  constructor(config, owner) {
+    super(config, owner);
+
+    this.endpoints.log.receive = async entry => {
+      journal_print(entry.severity + ":" + JSON.stringify(entry));
+    };
+  }
+}
 
 /**
  * Kronos bridge to systemd
@@ -11,7 +22,13 @@ const { notify } = require("../systemd.node");
  * - propagate socket activations into kronos
  * - start / stop / restart / reload initiated from systemd
  */
-export class ServiceSystemd extends ServiceProviderMixin(Service) {
+export class ServiceSystemd extends ServiceProviderMixin(Service, JournalLogger) {
+
+  constructor(...args) {
+    super(...args);
+    journal_print("Message to the journal");
+  }
+
   static get name() {
     return "systemd";
   }
--- src/systemd.cpp
+++ src/systemd.cpp
@@ -2,58 +2,102 @@
 #include <node_api.h>
 #include <systemd/sd-daemon.h>
 
-namespace daemon {
+namespace daemon
+{
 
-    napi_value booted(napi_env env, napi_callback_info) {
-        napi_status status;
-        napi_value value;
+napi_value booted(napi_env env, napi_callback_info)
+{
+    napi_status status;
+    napi_value value;
 
-        status = napi_create_int32(env, sd_booted(), &value);
-        if (status != napi_ok) return nullptr;
+    status = napi_create_int32(env, sd_booted(), &value);
+    if (status != napi_ok)
+        return nullptr;
 
-        return value;
-    }
-
-    napi_value notify(napi_env env, napi_callback_info info) {
-        napi_status status;
-        size_t argc = 1;
-        napi_value args[1];
-        status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
-        if (status != napi_ok) return nullptr;
-
-        if (argc != 1) {
-            napi_throw_error(env, nullptr, "Wrong arguments");
-        }
-
-        size_t len;
-        status = napi_get_value_string_utf8(env, args[0], nullptr, 0, &len);
-        if (status != napi_ok) return nullptr;
-
-        char* state = new char[len + 1];
-        status = napi_get_value_string_utf8(env, args[0], state, len + 1, nullptr);
-        int res = sd_notify(0, state);
-        delete[] state;
-
-        napi_value value;
-        status = napi_create_int32(env, res, &value);
-        if (status != napi_ok) return nullptr;
-
-        return value;
-    }
+    return value;
 }
 
-napi_value init(napi_env env, napi_value exports) {
+napi_value notify(napi_env env, napi_callback_info info)
+{
+    napi_status status;
+    size_t argc = 1;
+    napi_value args[1];
+    status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    if (status != napi_ok)
+        return nullptr;
+
+    if (argc != 1)
+    {
+        napi_throw_error(env, nullptr, "Wrong arguments");
+    }
+
+    size_t len;
+    status = napi_get_value_string_utf8(env, args[0], nullptr, 0, &len);
+    if (status != napi_ok)
+        return nullptr;
+
+    char *state = new char[len + 1];
+    status = napi_get_value_string_utf8(env, args[0], state, len + 1, nullptr);
+    int res = sd_notify(0, state);
+    delete[] state;
+
+    napi_value value;
+    status = napi_create_int32(env, res, &value);
+    if (status != napi_ok)
+        return nullptr;
+
+    return value;
+}
+
+napi_value journal_print(napi_env env, napi_callback_info info)
+{
+    napi_status status;
+    size_t argc = 1;
+    napi_value args[1];
+    status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    if (status != napi_ok)
+        return nullptr;
+
+    if (argc != 1)
+    {
+        napi_throw_error(env, nullptr, "Wrong arguments");
+    }
+
+    size_t len;
+    status = napi_get_value_string_utf8(env, args[0], nullptr, 0, &len);
+    if (status != napi_ok)
+        return nullptr;
+
+    char *message = new char[len + 1];
+    status = napi_get_value_string_utf8(env, args[0], message, len + 1, nullptr);
+    int res = sd_journal_print(LOG_INFO, message);
+    delete[] message;
+ 
+    napi_value value;
+    status = napi_create_int32(env, res, &value);
+    if (status != napi_ok)
+        return nullptr;
+
+    return value;
+}
+
+}
+
+napi_value init(napi_env env, napi_value exports)
+{
     napi_status status;
     napi_value listenFdsStart;
     status = napi_create_int32(env, SD_LISTEN_FDS_START, &listenFdsStart);
-    if (status != napi_ok) return nullptr;
+    if (status != napi_ok)
+        return nullptr;
     napi_property_descriptor desc[] = {
         {"LISTEN_FDS_START", nullptr, nullptr, nullptr, nullptr, listenFdsStart, napi_default, nullptr},
         {"booted", nullptr, daemon::booted, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"notify", nullptr, daemon::notify, nullptr, nullptr, nullptr, napi_default, nullptr}
-    };
+        {"notify", nullptr, daemon::notify, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"journal_print", nullptr, daemon::notify, nullptr, nullptr, nullptr, napi_default, nullptr}};
     status = napi_define_properties(env, exports, 3, desc);
-    if (status != napi_ok) return nullptr;
+    if (status != napi_ok)
+        return nullptr;
     return exports;
 }
 
