c389c14ee9688cc6241099af2eb3519ec5d2761a
Markus Felten <Markus.Felten@db.com>
2019-11-04 00:08:05 +0100

    test: wip

--- tests/notify-test-cli.mjs
+++ tests/notify-test-cli.mjs
@@ -1,9 +1,36 @@
+import { Service } from '@kronos-integration/service';
 import ServiceSystemd from '../src/service.mjs';
+
+class TestService extends Service {
+  static get name() {
+    return "test";
+  }
+
+  get autostart() {
+    return true;
+  }
+
+  async configure(config) {
+    delete config.name;
+    Object.assign(this, config);
+    wait(1000);
+
+    return this.restartIfRunning();
+  }
+}
 
 const ssd = new ServiceSystemd();
 
 async function actions()
 {
+  ssd.registerServiceFactory(TestService);
+  await ssd.declareService(
+     {
+        type: "test"
+      },
+      true
+    );
+
   ssd.info('starting...');
   await ssd.start();
   ssd.info('started...');
@@ -17,9 +44,9 @@ async function actions()
 
 actions();
 
-async function wait(msecs = 1000) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, msecs);
-  });
+async function wait(msecs=1000) {
+  return new Promise((resolve, reject) => setTimeout(() => resolve(), msecs));
 }
+
+
 
