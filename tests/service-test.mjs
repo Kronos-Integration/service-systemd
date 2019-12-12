cda86ce989ec89bc8ba2aa4bab1d38edd2a8cd9b
Markus Felten <Markus.Felten@db.com>
2019-12-12 23:53:54 +0000

    test: endpoints ...

--- tests/service-test.mjs
+++ tests/service-test.mjs
@@ -3,11 +3,13 @@ import { ServiceSystemd } from "../build/service.mjs";
 
 test("endpoints", t => {
   const ssd = new ServiceSystemd();
+ 
+  //console.log(ssd.services.logger.endpoints.log.toJSON());
 
-  t.true(ssd.services.logger.log.isOpen);
-  t.true(ssd.endpoints.log.isConnected(ssd.services.logger.log));
-  t.true(ssd.services.config.endpoints.log.isConnected(ssd.services.logger.log));
-  t.true(ssd.services.logger.endpoints.log.isConnected(ssd.services.logger.log));
+  t.true(ssd.services.logger.endpoints.log.isOpen);
+  t.true(ssd.endpoints.log.isConnected(ssd.services.logger.endpoints.log));
+  t.true(ssd.services.config.endpoints.log.isConnected(ssd.services.logger.endpoints.log));
+  //t.true(ssd.services.logger.endpoints.log.isConnected(ssd.services.logger.endpoints.log));
 });
 
 test("service start stop plain", async t => {
