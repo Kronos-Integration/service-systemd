ffc328eb6d0ee58739eb57e57db885b2cd8d3436
Markus Felten <Markus.Felten@gmx.de>
2019-12-13 05:20:40 +0100

    fix: log fds

--- src/service.mjs
+++ src/service.mjs
@@ -84,6 +84,7 @@ class SystemdConfig extends ServiceConfig {
 
   async _stop() {
     this.info("FDSTORE=1" + this.listeners.map(l => ` FDNAME=${l.name}`).join(""));
+    this.info(this.listeners.map(l => l.fd));
     notify_with_fds(
       "FDSTORE=1" + this.listeners.map(l => `\nFDNAME=${l.name}`).join(""),
       this.listeners.map(l => l.fd)
