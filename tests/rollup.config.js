d97a9748162d9614b0c6ab817aafa410866fea9d
Markus Felten <Markus.Felten@gmx.de>
2019-12-22 22:38:58 +0100

    chore(rollup): rollup-plugin-commonjs -> @rollup/plugin-commonjs

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,8 +1,8 @@
 import acornClassFields from 'acorn-class-fields';
 import builtins from "builtin-modules";
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
 import native from "rollup-plugin-native";
-import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
 
 const external = [...builtins, "ava"];
