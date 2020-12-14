da23804950092b0408f1df768d0faab08f167b7f
Markus Felten <Markus.Felten@gmx.de>
2020-12-14 15:32:45 +0100

    test: cleanup

--- tests/helpers/notify-test-cli.mjs
+++ tests/helpers/notify-test-cli.mjs
@@ -74,7 +74,7 @@ async function actions() {
   ssd.info("0123456789".repeat(5));
 
   for (let i = 0; i < 100; i++) {
-    await wait(500);
+    await wait(300);
     ssd.info(`waiting ${i}...`);
   }
 
