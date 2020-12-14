89dc2ee4c881a82094e6062c700f888662a38bca
Markus Felten <Markus.Felten@gmx.de>
2020-12-14 13:52:30 +0100

    chore(action): only install systemd

--- .github/workflows/ubuntu-build-check.sh
+++ .github/workflows/ubuntu-build-check.sh
@@ -15,54 +15,15 @@ ARGS=(
     "-Db_ndebug=true"
 )
 PACKAGES=(
-    cryptsetup-bin
-    expect
-    fdisk
-    gettext
-    iputils-ping
-    isc-dhcp-client
-    itstool
-    kbd
-    libblkid-dev
-    libcap-dev
-    libcurl4-gnutls-dev
-    libfdisk-dev
-    libgpg-error-dev
-    liblz4-dev
-    liblzma-dev
-    libmicrohttpd-dev
-    libmount-dev
-    libp11-kit-dev
-    libpwquality-dev
-    libqrencode-dev
-    libssl-dev
-    libxkbcommon-dev
-    libxtables-dev
-    libzstd-dev
     libsystemd-dev
-    mount
-    net-tools
-    perl
-    python-lxml
-    python3-evdev
-    python3-lxml
-    python3-pip
-    python3-pyparsing
-    python3-setuptools
-    quota
-    strace
-    unifont
-    util-linux
-    zstd
 )
-RELEASE="$(lsb_release -cs)"
+#RELEASE="$(lsb_release -cs)"
 
-bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ $RELEASE main restricted universe multiverse' >>/etc/apt/sources.list"
-
+#bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ $RELEASE main restricted universe multiverse' >>/etc/apt/sources.list"
 # PPA with some newer build dependencies (like zstd)
-add-apt-repository -y ppa:upstream-systemd-ci/systemd-ci
+#add-apt-repository -y ppa:upstream-systemd-ci/systemd-ci
 apt-get -y update
-apt-get -y build-dep systemd
-apt-get -y install "${PACKAGES[@]}"
-export PATH="$HOME/.local/bin:$PATH"
+apt-get -y systemd libsystemd-dev
+#apt-get -y install "${PACKAGES[@]}"
+#export PATH="$HOME/.local/bin:$PATH"
 
